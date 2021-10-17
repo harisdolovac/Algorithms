8b6d497da6b3d8dd1a27929857ee4fa9067be08e
harisdolovac <53434761+harisdolovac@users.noreply.github.com>
2021-10-17 17:34:43 +0200

    Function 1 - hello world

--- kata8.js
+++ kata8.js
@@ -23,8 +23,12 @@
 // }
 // console.log(validateHello("heLLO"));
 
-function even_or_odd(number) {
-  return number % 2 === 0 ? "Even" : "Odd";
-}
+// function even_or_odd(number) {
+//   return number % 2 === 0 ? "Even" : "Odd";
+// }
 
-console.log(even_or_odd(2));
+// console.log(even_or_odd(2));
+
+function greet() {
+  return "hello world!";
+}
