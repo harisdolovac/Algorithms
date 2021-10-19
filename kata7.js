797cdfe4b42854512def0ab251ec8f699161d3da
harisdolovac <53434761+harisdolovac@users.noreply.github.com>
2021-10-19 13:18:52 +0200

    New kata

--- kata7.js
+++ kata7.js
@@ -1,6 +1,21 @@
-function highAndLow(numbers) {
-  let nums = numbers.split(" ").sort((a, b) => a - b);
-  return [nums[0], nums[nums.length - 1]].join(" ");
+// function highAndLow(numbers) {
+//   let nums = numbers.split(" ").sort((a, b) => a - b);
+//   return [nums[0], nums[nums.length - 1]].join(" ");
+// }
+
+// console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+
+function disariumNumber(n) {
+  let nums = String(n);
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    let newA = Number(element);
+    sum += Math.pow(newA, i + 1);
+  }
+
+  return sum === n ? "Disarium !!" : "Not !!";
 }
 
-console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+console.log(disariumNumber(89));
